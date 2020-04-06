dc93de1840eaf70795e1030951bd88a4f9e89213
Sylver35 <33929219+Sylver35@users.noreply.github.com>
2020-04-06 18:24:25 +0200

    Add files via upload

--- controller/controller.php
+++ controller/controller.php
@@ -43,11 +43,11 @@ class controller
 	*/
 	public function __construct(config $config, request $request, user $user, language $language, manager $passwords_manager, $root_path, $php_ext)
 	{
-		$this->config 				= $config;
+		$this->config				= $config;
 		$this->request				= $request;
 		$this->user					= $user;
 		$this->language				= $language;
-		$this->passwords_manager 	= $passwords_manager;
+		$this->passwords_manager	= $passwords_manager;
 		$this->root_path			= $root_path;
 		$this->php_ext				= $php_ext;
 	}
@@ -60,12 +60,11 @@ class controller
 	*/
 	public function ajax()
 	{
-		$mode 		= $this->request->variable('mode', '');
-		$username 	= $this->request->variable('username', '', true);
-		$email 		= $this->request->variable('email', '', true);
-		$password1 	= $this->request->variable('password1', '', true);
-		$password2 	= $this->request->variable('password2', '', true);
-		$img_true	= 'icon_ajax_true.png';
+		$mode		= $this->request->variable('mode', '');
+		$username	= $this->request->variable('username', '', true);
+		$email		= $this->request->variable('email', '', true);
+		$password1	= $this->request->variable('password1', '', true);
+		$password2	= $this->request->variable('password2', '', true);
 
 		// Load needed language data
 		$this->language->add_lang('ajaxchecks', 'sylver35/ajaxchecks');
@@ -79,61 +78,25 @@ class controller
 		switch ($mode)
 		{
 			case 'usernamecheck':
-				// Verify the length of username before
-				$length = strlen($username);
-				if ($length < $this->config['min_name_chars'])// if username is too small
+				if ($this->verify_length($mode, $username))
 				{
-					$this->return_content($mode, 'TOO_SHORT_USERNAME');
 					break;
 				}
-				if ($length > $this->config['max_name_chars'])// if username is too long
-				{
-					$this->return_content($mode, 'TOO_LONG_USERNAME');
-					break;
-				}
-				// Check that the username given has not already been used and respect all the obligations
-				$checkresult = validate_username($username);
-				// Check if it the username is ok (false means it is)
-				if ($checkresult !== false)// if the username already exists, is banned or does not respect all the obligations
-				{
-					$this->return_content($mode, $checkresult . '_USERNAME');
-				}
-				else// if username doesn't exist and respect all the obligations
-				{
-					$this->return_content($mode, 'AJAX_CHECK_USERNAME_TRUE', $img_true, 2);
-				}
+				$this->verify_username($mode, $username);
 			break;
 
 			case 'usernamecur':
-				// Verify the length of username before
-				$length = strlen($username);
-				if ($length < $this->config['min_name_chars'])// if username is too small
+				if ($this->verify_length($mode, $username))
 				{
-					$this->return_content($mode, 'TOO_SHORT_USERNAME');
-					break;
-				}
-				else if ($length > $this->config['max_name_chars'])// if username is too long
-				{
-					$this->return_content($mode, 'TOO_LONG_USERNAME');
 					break;
 				}
 				// it's actual username?
 				if (utf8_case_fold_nfc($username) === utf8_case_fold_nfc($this->user->data['username']))
 				{
-					$this->return_content($mode, 'AJAX_CHECK_USERNAME_CUR', $img_true, 2);
+					$this->return_content($mode, 'AJAX_CHECK_USERNAME_CUR', 'icon_ajax_true.png', 2);
 					break;
 				}
-				// Check that the username given has not already been used
-				$checkresult = validate_username($username);
-				// Check if it the username is ok (false means it is)
-				if ($checkresult === false)// if username doesn't exist and respect all the obligations
-				{
-					$this->return_content($mode, 'AJAX_CHECK_USERNAME_TRUE', $img_true, 2);
-				}
-				else// if the username already exists, not banned or does not respect all the obligations
-				{
-					$this->return_content($mode, $checkresult . '_USERNAME');
-				}
+				$this->verify_username($mode, $username);
 			break;
 
 			case 'checkemail':
@@ -143,7 +106,7 @@ class controller
 				{
 					break;
 				}
-				if ($length < 8)// if email is too small
+				if ($length < 9)// if email is too small
 				{
 					$this->return_content($mode, 'TOO_SHORT_EMAIL');
 					break;
@@ -159,198 +122,48 @@ class controller
 				else if ($this->user->data['is_registered'] && (utf8_case_fold_nfc($email) === utf8_case_fold_nfc($this->user->data['user_email'])))
 				{
 					// Only in page profile & mode reg_details for the current email in use
-					$this->return_content($mode, 'AJAX_CHECK_EMAIL_CURRENT', $img_true, 2);
+					$this->return_content($mode, 'AJAX_CHECK_EMAIL_CURRENT', 'icon_ajax_true.png', 2);
 				}
 				else
 				{
-					$this->return_content($mode, 'AJAX_CHECK_EMAIL_TRUE_FIRST', $img_true, 2);
+					$this->return_content($mode, 'AJAX_CHECK_EMAIL_TRUE_FIRST', 'icon_ajax_true.png', 2);
 				}
 			break;
 
 			case 'passwordcheck':
-				// Check that the two passwords given are the same
-				// Verify the length before
-				$length1 = strlen($password1);
-				$length2 = strlen($password2);
-				if ($length1 < $this->config['min_pass_chars'])// if password1 is too small
+				if ($this->verify_password($mode, $password1, $password2))
 				{
-					$this->return_content($mode, 'TOO_SHORT_NEW_PASSWORD');
 					break;
 				}
-				// Since phpbb 3.3 no max_pass_chars
-				if ($this->config->offsetExists('max_pass_chars'))
-				{
-					if ($length1 > $this->config['max_pass_chars'])// if password1 is too long
-					{
-						$this->return_content($mode, 'TOO_LONG_NEW_PASSWORD');
-						break;
-					}
-				}
-				if ($length2 < $this->config['min_pass_chars'])// if password2 is too small
-				{
-					$this->return_content($mode, 'TOO_SHORT_PASSWORD_CONFIRM');
-					break;
-				}
-				// Since phpbb 3.3 no max_pass_chars
-				if ($this->config->offsetExists('max_pass_chars'))
-				{
-					if ($length2 > $this->config['max_pass_chars'])// if password2 is too long
-					{
-						$this->return_content($mode, 'TOO_LONG_PASSWORD_CONFIRM');
-						break;
-					}
-				}
-				if ($length1 !== $length1)// If the passwords have different lengths
-				{
-					$this->return_content($mode, 'AJAX_CHECK_PASSWORD_BIG');
-					break;
-				}
-				// Only in page i=profile&mode=reg_details for the current password in use
-				if ($this->user->data['is_registered'])
-				{
-					$same_password = $this->passwords_manager->check($password1, $this->user->data['user_password']);
-					if ($same_password)
-					{
-						$this->return_content($mode, 'SAME_PASSWORD_ERROR');
-						break;
-					}
-				}
-				// Check the password doesn't contain any illegal chars, etc.
-				$checkresult = validate_password($password1);
-				// Check if the password is ok (false means it is)
-				if ($checkresult === false)
-				{
-					// Check if the passwords are the same
-					if (utf8_case_fold_nfc($password1) === utf8_case_fold_nfc($password2))
-					{
-						// Passwords are the same, show a correct message
-						$this->return_content($mode, 'AJAX_CHECK_PASSWORD_TRUE', $img_true, 2);
-					}
-					else
-					{
-						// Passwords not the same, show the error
-						$this->return_content($mode, 'AJAX_CHECK_PASSWORD_FALSE');
-					}
-				}
-				else
-				{
-					// Failed the password validation
-					$this->return_content($mode, $checkresult . '_NEW_PASSWORD');
-				}
-			break;
-
-			case 'strength';
-				$length1 = strlen($password1);
-				if ($length1 < $this->config['min_pass_chars'])// if password1 is too small
-				{
-					$this->return_content($mode, 'TOO_SHORT_NEW_PASSWORD');
-					break;
-				}
-				// Since phpbb 3.3 no max_pass_chars
-				if ($this->config->offsetExists('max_pass_chars'))
-				{
-					if ($length1 > $this->config['max_pass_chars'])// Check if password is too long
-					{
-						$this->return_content($mode, 'TOO_LONG_NEW_PASSWORD');
-						break;
-					}
-				}
-				// Check if the password is ok (false means it is)
-				$validate = validate_password($password1);
-				if ($validate !== false)
-				{
-					// Failed the password validation
-					$this->return_content($mode, $validate . '_NEW_PASSWORD');
-				}
-				else
-				{
-					// Check the "strength" of the password and show an image accordingly
-					$strength = $this->check_password_strength($password1);
-					$this->return_content($mode, $strength['content'], $strength['image'], $strength['number']+2, false, $strength['title']);
-				}
+				$this->validation_password($mode, $password1, $password2);
 			break;
 
 			case 'passwordcur':
-				// Verify the length
-				$length = strlen($password1);
-				if ($length < $this->config['min_pass_chars'])// if password is too small
+				if ($this->verify_password($mode, $password1))
 				{
-					$this->return_content($mode, 'TOO_SHORT_NEW_PASSWORD');
 					break;
 				}
-				// Since phpbb 3.3 no max_pass_chars
-				if ($this->config->offsetExists('max_pass_chars'))
+				if ($this->validation_password($mode, $password1))
 				{
-					if ($length > $this->config['max_pass_chars'])// if password is too long
-					{
-						$this->return_content($mode, 'TOO_LONG_NEW_PASSWORD');
-						break;
-					}
-				}
-				// Only in page reg_details for the current password in use
-				if ($this->user->data['is_registered'])
-				{
-					$same_password = $this->passwords_manager->check($password1, $this->user->data['user_password']);
-					if ($same_password)
-					{
-						$this->return_content($mode, 'SAME_PASSWORD_ERROR');
-						break;
-					}
-				}
-				// Check the password doesn't contain any illegal chars, etc.
-				$checkresult = validate_password($password1);
-				// Check if it the password is ok (false means it is)
-				if ($checkresult !== false)
-				{
-					// Failed the password validation
-					$this->return_content($mode, $checkresult . '_NEW_PASSWORD');
 					break;
 				}
-				$check_password = $this->passwords_manager->check($password1, $this->user->data['user_password']);
-				// Check if it the password is ok (false means it is)
-				if ($check_password !== false)
-				{
-					$this->return_content($mode, 'SAME_PASSWORD_ERROR');
-				}
-				else
-				{
-					$this->return_content($mode, 'AJAX_CHECK_PASSWORD_OK', $img_true, 2);
-				}
+				$this->check_password($mode, $password1);
 			break;
 
 			case 'oldpassword':
-				// Now check both are the same
-				// Verify the length
-				$length = strlen($password1);
-				if ($length < $this->config['min_pass_chars'])// if password is too small
+				if ($this->verify_password($mode, $password1, $password2))
 				{
-					$this->return_content($mode, 'TOO_SHORT_NEW_PASSWORD');
 					break;
 				}
-				// Since phpbb 3.3 no max_pass_chars
-				if ($this->config->offsetExists('max_pass_chars'))
+				$this->check_password($mode, $password1);
+			break;
+
+			case 'strength';
+				if ($this->verify_password($mode, $password1))
 				{
-					if ($length > $this->config['max_pass_chars'])// if password is too long
-					{
-						$this->return_content($mode, 'TOO_LONG_NEW_PASSWORD');
-						break;
-					}
-				}
-				if (utf8_case_fold_nfc($password1) === utf8_case_fold_nfc($password2))
-				{
-					$this->return_content($mode, 'SAME_PASSWORD_ERROR');
 					break;
 				}
-				$check_password = $this->passwords_manager->check($password1, $this->user->data['user_password']);
-				// Check if it the password is ok (true means it is)
-				if ($check_password !== false)
-				{
-					$this->return_content($mode, 'AJAX_CHECK_PASSWORD_OK', $img_true, 2);
-				}
-				else
-				{
-					$this->return_content($mode, 'CUR_PASSWORD_ERROR');
-				}
+				$this->validation_password($mode, $password1, false, true);
 			break;
 		}
 	}
@@ -373,16 +186,221 @@ class controller
 		$response->send(array(
 			'mode'		=> $mode,
 			'content'	=> $this->language->lang($value),
-			'image'		=> (!$image) ? 'icon_ajax_false.png' : $image,
-			'type'		=> (!$type) ? 1 : $type,
-			'reason'	=> (!$reason) ? '' : $reason,
-			'strength'	=> (!$strength) ? false : $this->language->lang($strength),
+			'image'		=> ($image !== false) ? $image : 'icon_ajax_false.png',
+			'type'		=> ($type !== false) ? $type : 1,
+			'reason'	=> ($reason !== false) ? $reason : '',
+			'strength'	=> ($strength !== false) ? $this->language->lang($strength) : false,
 		), true);
 	}
 
 	/**
-	* check the "strength" of the password
-	* show an image and color text accordingly
+	* Verify the length of username
+	*
+	* @param string		$mode
+	* @param string		$username
+	* @return bool
+	* @access private
+	*/
+	private function verify_length($mode, $username)
+	{
+		$length = strlen($username);
+		// if username is too small
+		if ($length < $this->config['min_name_chars'])
+		{
+			$this->return_content($mode, 'TOO_SHORT_USERNAME');
+			return true;
+		}
+		// if username is too long
+		if ($length > $this->config['max_name_chars'])
+		{
+			$this->return_content($mode, 'TOO_LONG_USERNAME');
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	* Verify if username respect all the obligations
+	*
+	* @param string		$mode
+	* @param string		$username
+	* @return bool
+	* @access private
+	*/
+	private function verify_username($mode, $username)
+	{
+		// Check that the username given has not already been used
+		$checkresult = validate_username($username);
+		// Check if it the username is ok (false means it is)
+		// if the username already exists, not banned or does not respect all the obligations
+		if ($checkresult !== false)
+		{
+			$this->return_content($mode, $checkresult . '_USERNAME');
+			return true;
+		}
+		else
+		{
+			// if username doesn't exist and respect all the obligations
+			$this->return_content($mode, 'AJAX_CHECK_USERNAME_TRUE', 'icon_ajax_true.png', 2);
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	* Verify length of passwords
+	*
+	* @param string		$mode
+	* @param string		$password1
+	* @param string		$password2
+	* @return bool
+	* @access private
+	*/
+	private function verify_password($mode, $password1, $password2 = false)
+	{
+		$length1 = strlen($password1);
+		// if password1 is too small
+		if ($length1 < $this->config['min_pass_chars'])
+		{
+			$this->return_content($mode, 'TOO_SHORT_NEW_PASSWORD');
+			return true;
+		}
+		if ($mode == 'oldpassword')
+		{
+			// The two passwords are identical ?
+			if (utf8_case_fold_nfc($password1) === utf8_case_fold_nfc($password2))
+			{
+				// If the second is the same as the current one
+				$check_password = $this->passwords_manager->check($password2, $this->user->data['user_password']);
+				if ($check_password !== false)
+				{
+					$this->return_content($mode, 'SAME_PASSWORD_ERROR');
+					return true;
+				}
+			}
+		}
+		else if ($mode != 'strength')
+		{
+			if ($password2 !== false)
+			{
+				$length2 = strlen($password2);
+				// if password2 is too small
+				if ($length2 < $this->config['min_pass_chars'])
+				{
+					$this->return_content($mode, 'TOO_SHORT_PASSWORD_CONFIRM');
+					return true;
+				}
+				// If the passwords have different lengths
+				if ($length2 > $length1)
+				{
+					$this->return_content($mode, 'AJAX_CHECK_PASSWORD_BIG');
+					return true;
+				}
+			}
+			// Only in page reg_details for the current password in use
+			if ($this->user->data['is_registered'])
+			{
+				$same_password = $this->passwords_manager->check($password1, $this->user->data['user_password']);
+				if ($same_password)
+				{
+					$this->return_content($mode, 'SAME_PASSWORD_ERROR');
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	* Check the password doesn't contain any illegal chars etc.
+	*
+	* @param string		$mode
+	* @param string		$password
+	* @return bool
+	* @access private
+	*/
+	private function validation_password($mode, $password1, $password2 = false, $strenght = false)
+	{
+		$checkresult = validate_password($password1);
+		// Check if the password is ok (false means it is)
+		if ($checkresult !== false)
+		{
+			// Failed the password validation
+			$this->return_content($mode, $checkresult . '_NEW_PASSWORD');
+			return true;
+		}
+		else if ($strenght !== false)
+		{
+			// Check the "strength" of the password and show an image accordingly
+			$strength = $this->check_password_strength($password1);
+			$this->return_content($mode, $strength['content'], $strength['image'], $strength['number']+2, false, $strength['title']);
+			return true;
+		}
+		else if ($password2 !== false)
+		{
+			// Check if first and second passwords are the same
+			if (utf8_case_fold_nfc($password1) === utf8_case_fold_nfc($password2))
+			{
+				// Passwords are the same, show a correct message
+				$this->return_content($mode, 'AJAX_CHECK_PASSWORD_TRUE', 'icon_ajax_true.png', 2);
+				return true;
+			}
+			else
+			{
+				// Passwords not the same, show the error
+				$this->return_content($mode, 'AJAX_CHECK_PASSWORD_FALSE');
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	* Check if the password is ok
+	*
+	* @param string		$mode
+	* @param string		$password
+	* @return void
+	* @access private
+	*/
+	private function check_password($mode, $password)
+	{
+		$check_password = $this->passwords_manager->check($password, $this->user->data['user_password'], $this->user->data);
+		switch ($mode)
+		{
+			case 'passwordcur':
+				// Check if it the password is ok (false means it is)
+				if ($check_password !== false)
+				{
+					$this->return_content($mode, 'SAME_PASSWORD_ERROR');
+				}
+				else
+				{
+					$this->return_content($mode, 'AJAX_CHECK_PASSWORD_OK', 'icon_ajax_true.png', 2);
+				}
+			break;
+
+			case 'oldpassword':
+				// Check if it the password is ok (true means it is)
+				if ($check_password !== false)
+				{
+					$this->return_content($mode, 'AJAX_CHECK_PASSWORD_OK', 'icon_ajax_true.png', 2);
+				}
+				else
+				{
+					$this->return_content($mode, 'CUR_PASSWORD_ERROR');
+				}
+			break;
+		}
+	}
+
+	/**
+	* Check the "strength" of the password and show an image and color text accordingly
+	*
 	* @param string $password
 	* @return array
 	* @access private
